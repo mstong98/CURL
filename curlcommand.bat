7e832daa33433fbfd5c042ab58d73bba22e133d3
mstong98 <mstong98@gmail.com>
2022-01-10 13:27:58 -0700

    Update curlcommand.bat

--- curlcommand.bat
+++ curlcommand.bat
@@ -1,2 +1,1 @@
-
-curl -d '{"userName":"ytccbusiness@gmail.com", "password":"Systems!!123"}' -H "Content-Type: application/json" -X POST https://dev.stedi.me/login
+curl -d "@data.json" -X POST "https://dev.stedi.me/login"
